a5abfef1d2fd979aaf7b29c9826d8173d2876006
Andrew-Stephen-Jacobs <andrewjacobs@ufl.edu>
2020-11-08 16:26:41 -0500

    Install Babel & JS Styling

---
+++ src/main.js
@@ -0,0 +1,10 @@
+document.getElementById("boiling-temp").innerHTML = "212";
+document.getElementById("freezing-temp").innerHTML = "32";
+document.getElementById("water-temp").innerHTML = "300";
+
+setTimeout(function(){
+    document.getElementById("boiling").innerHTML = "The water is hot. I think it is boiling";
+    node.getElementById("boiling").classList.remove(hide);
+    node.getElementById("boiling").classList.add(show);
+}, 3000);
+
